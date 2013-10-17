10db95b12f0922f326add849682cb45059d90498
deathowl <csergo.balint@ustream.tv>
2013-10-17 10:31:17 +0200

    add logger

--- server.js
+++ server.js
@@ -1,28 +1,30 @@
 var dgram = require("dgram"),
     AirbrakeNotice = require("airbrake-notice"),
+    logger = require('./lib/logger')
     config = require('./lib/config');
 
 var server = dgram.createSocket("udp4");
 config.configFile(process.argv[2], function (config, oldConfig) {
+    l = new logger.Logger(config.log || {});
 
 
 server.on("error", function (err) {
-  console.log("server error:\n" + err.stack);
+  l.log("server error:\n" + err.stack, 'ERROR');
   server.close();
 });
 
 server.on("message", function (msg, rinfo) {
-  console.log("server got: " + msg + " from " +
-    rinfo.address + ":" + rinfo.port);
+  l.log("server got: " + msg + " from " +
+    rinfo.address + ":" + rinfo.port, 'DEBUG');
 });
 
 server.on("listening", function () {
   var address = server.address();
-  console.log("server listening " +
-      address.address + ":" + address.port);
+  l.log("server listening " +
+      address.address + ":" + address.port, 'INFO');
 });
 
-server.bind(config.daemonport);
+server.bind(config.daemonport || 99999);
 
 });
 
